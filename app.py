f37411faa7da90e53fe97cb6ed0d9d3f810a9cf4
Maxwell Stone <maxwell@kinetixml.com>
2023-01-29 02:56:49 -0500

    add video2mp3

--- app.py
+++ app.py
@@ -81,5 +81,53 @@ def yt2mp3():
 
     return ('', 200, headers)
 
+class AppURLOpener(urllib.request.FancyURLopener):
+    version = "Mozilla/5.0"
+
+@app.route("/video2mp3", methods = ['POST'])
+def video2mp3(request):
+    extension = 'wav'
+    url = request.json["video"]
+    print(url.split("/")[9])
+
+    filename= url.split("/")[-1]
+    opener = AppURLOpener()
+    response = opener.open(url)
+    outfile = open(f"/tmp/{filename}", "wb")
+    outfile.write(response.read())
+    
+    videoPath = f"/tmp/{filename}"
+    audioPath = f"/tmp/{filename}.{extension}"
+    cmd = f"ffmpeg -i {videoPath} -vn {audioPath}"
+    os.system(cmd)
+
+    bucket = "hoya-hacks-video-files"
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket, timeout = 60)
+
+    file = bucket.blob(f"{filename}.{extension}")
+    file.upload_from_filename(audioPath)
+
+    outfile.close()
+
+    if request.method == 'POST':
+            headers = {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Max-Age': '3600'
+            }
+
+            return("gs://hoya-hacks-video-files/" + f"{filename}.{extension}", 200, headers)
+
+    headers = {
+        'Content-Type':'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+    }
+
+    return ('', 200, headers)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
