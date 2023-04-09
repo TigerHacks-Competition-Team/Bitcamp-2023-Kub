f66e0b88c8608c997ae55cc97f21cde1aa45ac76
Maxwell Stone <maxwell@kinetixml.com>
2023-04-09 00:52:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def wav2piano():
 
     # run spleeter
     print("running spleeter")
-    cmd = ["sudo", "spleeter", "separate", "--verbose", "-p", "spleeter:5stems", "--mwf", "-o", "/output/", "./original.mp3"]
+    cmd = ["sudo", "spleeter", "separate", "--verbose", "-p", "spleeter:5stems", "--mwf", "-o", "output/", "./original.mp3"]
     subprocess.Popen(cmd).wait(timeout=None)
 
     try:
@@ -148,17 +148,17 @@ def wav2piano():
     except:
         print("printing list dir failed for one of them")
 
-    subprocess.Popen(["ffmpeg", "-i", "/output/original/vocals.wav", "-b:a", "96k", "-acodec", "mp3", "/output/original/vocals.mp3"]).wait(timeout=None)
-    subprocess.Popen(["ffmpeg", "-i", "/output/original/piano.wav", "-b:a", "96k", "-acodec", "mp3", "/output/original/piano.mp3"]).wait(timeout=None)
-    subprocess.Popen(["ffmpeg", "-i", "/output/original/drums.wav", "-b:a", "96k", "-acodec", "mp3", "/output/original/bass.mp3"]).wait(timeout=None)
-    subprocess.Popen(["ffmpeg", "-i", "/output/original/bass.wav", "-b:a", "96k", "-acodec", "mp3", "/output/original/drums.mp3"]).wait(timeout=None)
-    subprocess.Popen(["ffmpeg", "-i", "/output/original/other.wav", "-b:a", "96k", "-acodec", "mp3", "/output/original/other.mp3"]).wait(timeout=None)
+    subprocess.Popen(["ffmpeg", "-i", "output/original/vocals.wav", "-b:a", "96k", "-acodec", "mp3", "output/original/vocals.mp3"]).wait(timeout=None)
+    subprocess.Popen(["ffmpeg", "-i", "output/original/piano.wav", "-b:a", "96k", "-acodec", "mp3", "output/original/piano.mp3"]).wait(timeout=None)
+    subprocess.Popen(["ffmpeg", "-i", "output/original/drums.wav", "-b:a", "96k", "-acodec", "mp3", "output/original/bass.mp3"]).wait(timeout=None)
+    subprocess.Popen(["ffmpeg", "-i", "output/original/bass.wav", "-b:a", "96k", "-acodec", "mp3", "output/original/drums.mp3"]).wait(timeout=None)
+    subprocess.Popen(["ffmpeg", "-i", "output/original/other.wav", "-b:a", "96k", "-acodec", "mp3", "output/original/other.mp3"]).wait(timeout=None)
 
-    vocals_path = "/output/original/vocals.mp3"
-    piano_path = "/output/original/piano.mp3"
-    drums_path = "/output/original/drums.mp3"
-    bass_path = "/output/original/bass.mp3"
-    other_path = "/output/original/other.mp3"
+    vocals_path = "output/original/vocals.mp3"
+    piano_path = "output/original/piano.mp3"
+    drums_path = "output/original/drums.mp3"
+    bass_path = "output/original/bass.mp3"
+    other_path = "output/original/other.mp3"
 
     vocals_storage = f"songs/{docID}/vocals.mp3"
     piano_storage = f"songs/{docID}/piano.mp3"
